880b7381f1f48321bea9489c22bae80a1f1e7de1
Mike Nöthiger <noethiger.mike@gmail.com>
2019-12-28 02:44:29 +0100

    minesweeper accepted by uva

--- 10189_Minescweeper/main.c
+++ 10189_Minescweeper/main.c
@@ -47,58 +47,52 @@ char read_char() {
 	return buf;
 }
 
-bool is_adjacent(int a, int b, int n, int m, int moves[8]) {
-	// move indexes:
+/**
+ * Check neighbourship of a with s, s being
+ *
+ * @param a
+ * @param s
+ * @param n
+ * @param m
+ * @param moves
+ * @return
+ */
+bool is_neighbour(int a, int s, int n, int m, int moves[8]) {
+	// neighbour indexes of x:
 	// 4 1 5
 	// 0 x 2
 	// 7 3 6
-	int invalid_positions[8] = {0,0,0,0,0,0,0,0};
-	int pos = 0;
+	int neighbours[8] = {1,1,1,1,1,1,1,1};
 	// left edge
 	if (a % m == 0) {
-		invalid_positions[4] = 1;
-		invalid_positions[0] = 1;
-		invalid_positions[7] = 1;
+		neighbours[4] = 0;
+		neighbours[0] = 0;
+		neighbours[7] = 0;
 	}
 	// right edge
 	if ((a+1) % m == 0) {
-//		printf("right edge\n");
-		invalid_positions[5] = 1;
-		invalid_positions[2] = 1;
-		invalid_positions[6] = 1;
+		neighbours[5] = 0;
+		neighbours[2] = 0;
+		neighbours[6] = 0;
 	}
 	// top edge
 	if (a/m == 0) {
-		invalid_positions[4] = 1;
-		invalid_positions[1] = 1;
-		invalid_positions[5] = 1;
+		neighbours[4] = 0;
+		neighbours[1] = 0;
+		neighbours[5] = 0;
 	}
 	// bottom edge
 	if (a/m == (n-1)) {
-		invalid_positions[7] = 1;
-		invalid_positions[3] = 1;
-		invalid_positions[6] = 1;
+		neighbours[7] = 0;
+		neighbours[3] = 0;
+		neighbours[6] = 0;
 	}
-	int i, index;
-	for (i = 0; i < 8; i++) {
-		if (invalid_positions[i] && a+moves[i] == b) return false;
-	}
-	return true;
-}
-
-void print_field(char field[][2], int n, int m) {
-	int s = m*n;
-	int i;
-	for (i = 0; i < s; i++) {
-		if (i != 0 && i % m == 0) printf("\n");
-		printf("%c", field[i][1]);
-	}
-	printf("\n");
+	return neighbours[s];
 }
 
 void solve(int n, int m) {
 	int s = n*m;
-	int i;
+	int i, j;
 	char buf;
 	char field[s][2];
 
@@ -110,22 +104,11 @@ void solve(int n, int m) {
 	int m_r_u = m_r + m_u;
 	int m_l_d = m_l+ m_d;
 	int m_r_d = m_r + m_d;
-	// move indexes:
+	// adjacence indexes relative to index x:
 	// 4 1 5
 	// 0 x 2
 	// 7 3 6
 	int moves[8] = { m_l, m_u, m_r, m_d, m_l_u, m_r_u, m_r_d, m_l_d };
-
-	int pivot = 1;
-	printf("pivot: %d\n", pivot);
-	printf("left: %d\n", pivot+moves[0]);
-	printf("top: %d\n", pivot+moves[1]);
-	printf("right: %d\n", pivot+moves[2]);
-	printf("bottom: %d\n", pivot+moves[3]);
-	printf("top-left: %d\n", pivot+moves[4]);
-	printf("top-right: %d\n", pivot+moves[5]);
-	printf("bottom-right: %d\n", pivot+moves[6]);
-	printf("bottom-left: %d\n", pivot+moves[7]);
 
 	// read field input data
 	for (i = 0; i < s; i++) {
@@ -133,20 +116,7 @@ void solve(int n, int m) {
 		field[i][1] = '0';
 	}
 
-	int a = 1, b;
-	for (i = 0; i < 8; i++) {
-		b = a + moves[i];
-		if (is_adjacent(a, b, n, m, moves)) {
-			field[b][1] = 'a';
-		} else {
-			field[b][1] = 'n';
-		}
-	}
-	printf("pivot=%d\n", a);
-	print_field(field, n, m);
-	printf("\n");
-
-	int j, k, mines;
+	int k, mines;
 	for (i = 0; i < s; i++) {
 		if (i > 0 && i % m == 0) printf("\n");
 		if (field[i][0] == '*') {
@@ -155,7 +125,9 @@ void solve(int n, int m) {
 			mines = 0;
 			for (j = 0; j < 8; j++) {
 				k = i + moves[j];
-				if (is_adjacent(i, k, n, m, moves) && field[k][0] == '*') mines++;
+				if (is_neighbour(i, j, n, m, moves) && field[k][0] == '*') {
+					mines++;
+				}
 			}
 			printf("%d", mines);
 		}
@@ -166,9 +138,10 @@ int main() {
 	int n, m, f = 1;
 	while ((n = read_num(NULL))) {
 		m = read_num(NULL);
+		if (f > 1) printf("\n");
 		printf("Field #%d:\n", f++);
 		solve(n,m);
-		printf("\n\n");
+		printf("\n");
 	}
 	return 0;
 }
--- 10189_Minescweeper/main2.c
+++ 10189_Minescweeper/main2.c
@@ -35,33 +35,58 @@ char read_char() {
 }
 
 bool is_adjacent(int a, int b, int n, int m, int moves[8]) {
-	int invalid_positions[8] = {0,0,0,0,0,0,0,0};
-	int pos = 0;
+	if (b < 0 || b >= m*n) return false;
+	int allowed_moves[8] = {1,1,1,1,1,1,1,1};
 	if (a % m == 0) {
-		invalid_positions[4] = 1;
-		invalid_positions[0] = 1;
-		invalid_positions[7] = 1;
+		allowed_moves[4] = 0;
+		allowed_moves[0] = 0;
+		allowed_moves[7] = 0;
 	}
 	if ((a+1) % m == 0) {
-		invalid_positions[5] = 1;
-		invalid_positions[2] = 1;
-		invalid_positions[6] = 1;
+		allowed_moves[5] = 0;
+		allowed_moves[2] = 0;
+		allowed_moves[6] = 0;
 	}
 	if (a/m == 0) {
-		invalid_positions[4] = 1;
-		invalid_positions[1] = 1;
-		invalid_positions[5] = 1;
+		allowed_moves[4] = 0;
+		allowed_moves[1] = 0;
+		allowed_moves[5] = 0;
 	}
 	if (a/m == (n-1)) {
-		invalid_positions[7] = 1;
-		invalid_positions[3] = 1;
-		invalid_positions[6] = 1;
+		allowed_moves[7] = 0;
+		allowed_moves[3] = 0;
+		allowed_moves[6] = 0;
 	}
-	int i;
+	int i, p, j;
 	for (i = 0; i < 8; i++) {
-		if (invalid_positions[i] && a+moves[i] == b) return false;
+		if (allowed_moves[i] && a+moves[i] == b) return true;
 	}
-	return true;
+	return false;
+}
+
+bool is_neighbour(int a, int s, int n, int m, int moves[8]) {
+	int neighbours[8] = {1,1,1,1,1,1,1,1};
+	if (a % m == 0) {
+		neighbours[4] = 0;
+		neighbours[0] = 0;
+		neighbours[7] = 0;
+	}
+	if ((a+1) % m == 0) {
+		neighbours[5] = 0;
+		neighbours[2] = 0;
+		neighbours[6] = 0;
+	}
+	if (a/m == 0) {
+		neighbours[4] = 0;
+		neighbours[1] = 0;
+		neighbours[5] = 0;
+	}
+	if (a/m == (n-1)) {
+		neighbours[7] = 0;
+		neighbours[3] = 0;
+		neighbours[6] = 0;
+	}
+	return neighbours[s];
 }
 
 void print_field(char field[][2], int n, int m) {
@@ -76,7 +101,7 @@ void print_field(char field[][2], int n, int m) {
 
 void solve(int n, int m) {
 	int s = n*m;
-	int i;
+	int i, j;
 	char buf;
 	char field[s][2];
 
@@ -95,7 +120,7 @@ void solve(int n, int m) {
 		field[i][1] = '0';
 	}
 
-	int j, k, mines;
+	int k, mines;
 	for (i = 0; i < s; i++) {
 		if (i > 0 && i % m == 0) printf("\n");
 		if (field[i][0] == '*') {
@@ -104,7 +129,9 @@ void solve(int n, int m) {
 			mines = 0;
 			for (j = 0; j < 8; j++) {
 				k = i + moves[j];
-				if (is_adjacent(i, k, n, m, moves) && field[k][0] == '*') mines++;
+				if (is_neighbour(i, j, n, m, moves) && field[k][0] == '*') {
+					mines++;
+				}
 			}
 			printf("%d", mines);
 		}
@@ -115,9 +142,10 @@ int main() {
 	int n, m, f = 1;
 	while ((n = read_num(NULL))) {
 		m = read_num(NULL);
+		if (f > 1) printf("\n");
 		printf("Field #%d:\n", f++);
 		solve(n,m);
-		printf("\n\n");
+		printf("\n");
 	}
 	return 0;
 }
