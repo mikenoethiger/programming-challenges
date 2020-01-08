afc59d37879630893e92e1e3dcd676794b94a6cb
Mike Nöthiger <noethiger.mike@gmail.com>
2020-01-08 16:59:56 +0100

    cleanup

--- 10051_Tower_of_Cubes/main2.c
+++ 10051_Tower_of_Cubes/main2.c
@@ -11,10 +11,6 @@
 
 char *faces[] = {"front", "back", "left", "right", "top", "bottom"};
 
-/*
- * DATA STRUCTURES
- */
-
 typedef struct part_solution {
 	/* if cube above is equal to the cube represented in this solution,
 	 * the end of tower is reached */
@@ -23,64 +19,12 @@ typedef struct part_solution {
 	int height; // the height of this solution
 } PartSolution;
 
-typedef struct stack_element {
-	void *value;
-	struct stack_element *prev;
-} StackElement;
-
-typedef struct stack {
-	StackElement *top;
-} Stack;
-
 PartSolution *part_solution_create(int cube, int side, int height) {
 	PartSolution *part_solution = malloc(sizeof(PartSolution));
 	part_solution->cube = cube;
 	part_solution->side = side;
 	part_solution->height = height;
 	return part_solution;
-}
-
-StackElement *stack_element_create(void *value) {
-	StackElement *stack_element = malloc(sizeof(StackElement));
-	stack_element->value = value;
-	stack_element->prev = NULL;
-	return stack_element;
-}
-
-void stack_element_delete(StackElement *stack_element) {
-	free(stack_element);
-}
-
-Stack *stack_create() {
-	Stack *stack = malloc(sizeof(Stack));
-	stack->top = NULL;
-	return stack;
-}
-
-void stack_delete(Stack *stack) {
-	StackElement *stack_element = stack->top;
-	while (stack_element) {
-		stack_element_delete(stack_element);
-		stack_element = stack_element->prev;
-	}
-	free(stack);
-}
-
-void stack_push(Stack *stack, void *value) {
-	StackElement *stack_element = stack_element_create(value);
-	if (stack->top != NULL) {
-		stack_element->prev = stack->top;
-	}
-	stack->top = stack_element;
-}
-
-void *stack_pop(Stack *stack) {
-	if (stack->top == NULL) return NULL;
-	StackElement *stack_element = stack->top;
-	void *value = stack_element->value;
-	stack->top = stack_element->prev;
-	stack_element_delete(stack_element);
-	return value;
 }
 
 /*
@@ -92,40 +36,6 @@ int opposite_side(int s) {
 }
 
 /**
- * Reads a number from STDIN as follows:
- * - Leading white spaces, line breaks and tabs will be skipped.
- * - A leading '0' will be returned immediately.
- * - A leading '-' will multiply the subsequent number with -1.
- * - The first character from STDIN that is not '0' <= c <= '9'
- *   will terminate the algorithm. This character will be written
- *   to the out parameter t.
- *
- * @param t (nullable) out parameter that will hold the terminating
- *          character.
- * @return the number or zero if not a number
- */
-int read_num(char *t) {
-	char buf;
-	int num = 0;
-	int it = -1;
-	int coeff = 1;
-	while (read(STDIN_FILENO, &buf, sizeof(buf))) {
-		it++;
-		if (t != NULL) *t = buf;
-		if (num == 0 && (buf == ' ' || buf == '\n' || buf == '\t')) continue;
-		if (num == 0 && buf == '0') break;
-		if (it == 0 && buf == '-') {
-			coeff = -1;
-			continue;
-		}
-		if (!('0' <= buf && buf <= '9')) break;
-		num *= 10;
-		num += buf - '0';
-	}
-	return num * coeff;
-}
-
-/**
  * Read cube data from STDIN and write
  * the color value of each side to buf
  * which has to be an int array of size 6.
@@ -134,10 +44,6 @@ int read_num(char *t) {
  * @return
  */
 void read_cube(int *buf) {
-//	int i;
-//	for (i = 0; i < 6; i++) {
-//		buf[i] = read_num(NULL);
-//	}
 	scanf("%d%d%d%d%d%d", buf, buf+1, buf+2, buf+3, buf+4, buf+5);
 }
 
@@ -276,9 +182,7 @@ int main() {
 	int N;
 	int c = 1;
 	while (scanf("%d", &N) != EOF) {
-//		while ((N = read_num(NULL))) {
 		printf("Case #%d\n", c++);
-//		printf("num: %d\n", N);
 		calc_tallest_tower(N);
 		printf("\n");
 	}
